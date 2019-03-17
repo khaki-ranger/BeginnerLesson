20ac3334fdc431ed65c9a50e25cb81ae9a622b50
Terashima <yamanogomora@gmail.com>
2019-03-17 16:38:38 +0900

    Add: 画像の読み込みと画面をタップして画像を変更する処理

--- BeginnerLesson/ViewController.swift
+++ BeginnerLesson/ViewController.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     // MARK: properties
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var testImage: UIImageView!
+    let image1: UIImage = UIImage(named: "imagesForXCode/review_woman_star2.png")!
+    let image2: UIImage = UIImage(named: "imagesForXCode/review_woman_star5.png")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        testImage.image = image1
+        testImage.isUserInteractionEnabled = true
+        let tapAction:UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(ViewController.tapped(_:))
+        )
+        tapAction.delegate = self
+        self.view.addGestureRecognizer(tapAction)
+    }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer) {
+        if testImage.image == image1 {
+            testImage.image = image2
+        } else {
+            testImage.image = image1
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +43,6 @@ class ViewController: UIViewController {
     }
 
     // MARK: methods
-    @IBAction func changeLabel(_ sender: UIButton) {
-        self.testLabel.text = "Swift!"
-    }
     
 }
 
