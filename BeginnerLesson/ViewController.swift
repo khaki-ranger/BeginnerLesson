e26006783769d5099401039af021ab669133696a
Terashima <yamanogomora@gmail.com>
2019-03-17 11:41:02 +0900

    Add: ラベルとボタンを配置して、ボタン押下でラベルの文字を変える

--- BeginnerLesson/ViewController.swift
+++ BeginnerLesson/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: properties
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: methods
+    @IBAction func changeLabel(_ sender: UIButton) {
+        self.testLabel.text = "Swift!"
+    }
+    
 }
 
